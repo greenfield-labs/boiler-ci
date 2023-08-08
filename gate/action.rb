8d37d0112849d4b386e188fff6844504acfc0df7
greenfieldz <104225864+greenfieldz@users.noreply.github.com>
2023-08-08 18:46:01 -0400

    fix: add debuggery

--- gate/action.rb
+++ gate/action.rb
@@ -3,9 +3,11 @@ require 'http'
 
 response = HTTP.headers(
   'x-boiler-client-id': ENV["BOILER_CLIENT_ID"],
-  'x-boiler-client-secret': ENV["BOILER_CLIENT_SECRET"]
+  'x-boiler-client-secret': ENV["BOILER_CLIENT_SECRET"],
+  'x-github-oidc-url': ENV["ACTIONS_ID_TOKEN_REQUEST_URL"],
+  'x-github-oidc-token-request-token': ENV["ACTIONS_ID_TOKEN_REQUEST_TOKEN"]
 ).get(
   "https://boiler.ngrok.dev/api/gate/approve"
 )
 
-binding.pry
+puts response.body
