fd3930bb3627ca913baf939aa28d8533eb0e3d07
Thomas Vendetta <thomas@vendetta.io>
2023-08-08 22:44:41 -0400

    lazy pushing 2023-08-08 22:44:41

--- gate/action.rb
+++ gate/action.rb
@@ -4,12 +4,8 @@ require 'http'
 exchange = HTTP.headers(
   "Authorization": "Bearer #{ENV["ACTIONS_ID_TOKEN_REQUEST_TOKEN"]}"
 ).get(
-  ENV["ACTIONS_ID_TOKEN_REQUEST_URL"],
-  params: {
-    audience: "foobar"
-  }
+  ENV["ACTIONS_ID_TOKEN_REQUEST_URL"]
 )
-
 
 puts "EXCHANGE: #{exchange.code}"
 exchange_body = exchange.body
@@ -18,8 +14,7 @@ response = HTTP.headers(
   'x-boiler-client-id': ENV["BOILER_CLIENT_ID"],
   'x-boiler-client-secret': ENV["BOILER_CLIENT_SECRET"],
   'x-boiler-exchange': exchange_body,
-  'x-github-oidc-url': ENV["ACTIONS_ID_TOKEN_REQUEST_URL"],
-  'x-github-oidc-token-request-token': ENV["ACTIONS_ID_TOKEN_REQUEST_TOKEN"]
+  'x-github-oidc-token': JSON.parse(exchange_body)['value']
 ).get(
   "https://boiler.ngrok.dev/api/gate/approve"
 )
