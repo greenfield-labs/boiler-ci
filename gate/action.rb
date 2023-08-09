52ac16ea0cd1ea502e3fc2a97f5bf9629240f6dc
greenfieldz <104225864+greenfieldz@users.noreply.github.com>
2023-08-08 21:03:09 -0400

    lazy pushing 2023-08-08 21:03:09

--- gate/action.rb
+++ gate/action.rb
@@ -1,6 +1,15 @@
 require 'pry'
 require 'http'
 
+exchange = HTTP.headers(
+  "Authorization": "Bearer #{ENV["ACTIONS_ID_TOKEN_REQUEST_TOKEN"]}"
+).get(
+  ENV["ACTIONS_ID_TOKEN_REUQEST_URL"]
+)
+
+puts "EXCHANGE: #{exchange.code}"
+puts "EXCHANGE body: #{exchange.body}"
+
 response = HTTP.headers(
   'x-boiler-client-id': ENV["BOILER_CLIENT_ID"],
   'x-boiler-client-secret': ENV["BOILER_CLIENT_SECRET"],
@@ -9,8 +18,6 @@ response = HTTP.headers(
 ).get(
   "https://boiler.ngrok.dev/api/gate/approve"
 )
-
-puts "response code: #{response.code}"
 
 puts "ENV: #{ENV.to_h}"
 
