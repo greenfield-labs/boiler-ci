069f3251bb654c716395311707e82552472faa17
Thomas Vendetta <thomas@vendetta.io>
2023-09-08 17:26:00 -0400

    fix: update for token exchange

--- gate/action.rb
+++ gate/action.rb
@@ -1,31 +1,63 @@
-require 'pry'
 require 'http'
 require 'pathname'
 
-if ENV["BOILER_LOCAL"]
-  puts "Using local Boiler JWT"
+class Authentication
+  def authorize!
+    HTTP
+      .headers(
+        'content-type': 'application/json',
+        'x-github-oidc-token': client_token
+      )
+      .post("https://boiler.ngrok.dev/api/github/authorize")
+  end
 
-  jwt_path = Pathname.pwd.join("../../backend/.jwt")
-  jwt_contents = File.read(jwt_path).chomp
-else
-  puts "Using GitHub JWT exchange"
+  protected
 
-  exchange = HTTP.headers(
-    "Authorization": "Bearer #{ENV["ACTIONS_ID_TOKEN_REQUEST_TOKEN"]}"
-  ).get(
-    ENV["ACTIONS_ID_TOKEN_REQUEST_URL"]
-  )
+  # client_token either comes from GitHub when running in an action,
+  # or it can be read from disk for development purposes.
+  def client_token
+    @token ||= ENV["BOILER_LOCAL"] ? get_local_jwt : get_github_jwt
+  end
 
-  puts "EXCHANGE: #{exchange.code}"
-  exchange_body = exchange.body.to_s
-  jwt_contents = JSON.parse(exchange_body)['value']
+  def get_local_jwt
+    puts "Using local JWT"
+
+    jwt_path = Pathname.pwd.join("../../backend/.jwt")
+
+    File.read(jwt_path).chomp
+  end
+
+  def get_github_jwt
+    puts "Using GitHub JWT"
+
+    exchange = HTTP.headers(
+      "Authorization": "Bearer #{ENV["ACTIONS_ID_TOKEN_REQUEST_TOKEN"]}"
+    ).get(
+      ENV["ACTIONS_ID_TOKEN_REQUEST_URL"]
+    )
+
+    exchange_body = exchange.body.to_s
+
+    JSON.parse(exchange_body)['value']
+  end
 end
 
-response = HTTP.headers(
-  'content-type': 'application/json',
-  'x-github-oidc-token': jwt_contents
-).get(
-  "https://boiler.ngrok.dev/api/gate/approve"
-)
+def run
+  auth = Authentication.new.authorize!
 
-puts response.body
+  response = HTTP.headers(
+    'content-type': 'application/json',
+    'Authentication': auth.token
+  ).get(
+    "https://boiler.ngrok.dev/api/gate/approve"
+  )
+
+  puts response.body
+end
+
+if ARGV.empty?
+  run
+else
+  require 'pry';
+  binding.pry
+end
